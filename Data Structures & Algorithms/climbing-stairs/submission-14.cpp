28796d33b67f3f80d8750d3d036f7599a218d918
Ethan F <37308053+3zik@users.noreply.github.com>
2026-06-18 14:57:41 -0400

    Add: climbing-stairs - submission-14

---
+++ Data Structures & Algorithms/climbing-stairs/submission-14.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> memo;
+    int climbStairs(int n) {
+        memo.resize(n, -1);
+        return dfs(n, 0);
+    }
+    int dfs(int n, int i){
+        if (i == n) return 1;
+        if (i > n) return 0;
+
+        if (memo[i] != -1) return memo[i];
+        return memo[i] = dfs(n, i + 1) + dfs(n, i + 2); 
+    }
+};
