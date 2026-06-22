9966160782d22a932b3334c2b6f5bda7f4dbeaf7
Ethan F <37308053+3zik@users.noreply.github.com>
2026-06-22 10:39:08 -0400

    Add: anagram-groups - submission-9

---
+++ Data Structures & Algorithms/anagram-groups/submission-9.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> hash;
+        vector<vector<string>> res;
+
+        for (string s : strs){
+            vector<int> count(26,0);
+            for (char c : s){
+                count[c - 'a']++;
+            }
+            string key = "";
+            for (int i = 0; i < 26; ++i){
+                key += '#'+count[i];
+            }
+            hash[key].push_back(s);
+        }
+        for (const auto& pair : hash){
+            res.push_back(pair.second);
+        }
+        return res;
+    }
+};
