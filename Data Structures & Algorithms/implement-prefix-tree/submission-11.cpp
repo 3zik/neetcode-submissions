b17a4d8162499193a23f117fbfdfdab35e990ecc
Ethan F <37308053+3zik@users.noreply.github.com>
2026-06-25 11:56:47 -0400

    Add: implement-prefix-tree - submission-11

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-11.cpp
@@ -0,0 +1,47 @@
+class TrieNode{
+public:
+    std::unordered_map<char, TrieNode*> children;
+    bool endOfWord = false;
+};
+
+class PrefixTree {
+public:
+    TrieNode* root;
+    
+    PrefixTree() {
+        root = new TrieNode();
+    }
+    
+    void insert(string word) {
+        TrieNode* curr = root;
+        for (char c : word){
+            if (!curr->children.contains(c)){
+                curr->children[c] = new TrieNode();
+            }
+            curr = curr->children[c];
+        }
+        curr->endOfWord = true;
+    }
+    
+    bool search(string word) {
+        TrieNode* curr = root;
+        for (char c : word){
+            if (!curr->children.contains(c)){
+                return false;
+            }
+            curr = curr->children[c];
+        }
+        return curr->endOfWord;
+    }
+    
+    bool startsWith(string prefix) {
+        TrieNode* curr = root;
+        for (char c : prefix){
+            if (!curr->children.contains(c)){
+                return false;
+            }
+            curr = curr->children[c];
+        }
+        return true;
+    }
+};
